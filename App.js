85d01ad920e2ac0eee9935a9d12907284cfa25fd
AzizGerges <201927170@std.sci.cu.edu.eg>
2022-04-29 15:18:58 +0200

    edit app.js

--- App.js
+++ App.js
@@ -1,46 +1,20 @@
-import { onAuthStateChanged } from "firebase/auth";
-import Login from "./Components/Users/Login";
-import Register from "./Components/Users/Register";
-import { auth } from "./db/Config";
-import { useState, useEffect } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {View,Text,Button} from "react-native"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
 export default function App() {
-
-  const Stack = createNativeStackNavigator();
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => setUser(user));
-
-    return () => {
-      unsub();
-    };
-  }, []);
-function LogOrSign({navigation}){
-return(
-  <View>
-    <View style={{padding:20}}>
-    <Button title="Log In " onPress={()=>{ navigation.navigate('Log In')}}/>
-    </View>
-    <View style={{padding:20}}>
-    <Button  title="Register" onPress={()=>{ navigation.navigate('Register')}}/>
-    </View>
-  </View>
-
-);
-}
-  const [user, setUser] = useState(undefined);
   return (
- 
-<NavigationContainer>
-
-    <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={LogOrSign}           
-          options={{ title: 'My Chat App' }} />
-          {/* <Stack.Screen name="chat" component={CitiesList} /> */}
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Log In" component={Login} /> 
-      </Stack.Navigator> 
-  </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
