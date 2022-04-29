149d2ed50b9edc10e65058f1e0c2220b7a99ae85
AzizGerges <201927170@std.sci.cu.edu.eg>
2022-04-29 21:36:00 +0200

    Drinks design

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import Login from "./Components/Users/Login";
 import Register from "./Components/Users/Register";
 import { auth } from "./db/Config";
 import { useState, useEffect } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {View,Text,Button} from "react-native"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Button } from "react-native";
+import drinks from "./Components/Categories/import_drinks";
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => setUser(user));
@@ -16,30 +16,41 @@ export default function App() {
       unsub();
     };
   }, []);
-function LogOrSign({navigation}){
-return(
-  <View>
-    <View style={{padding:20}}>
-    <Button title="Log In " onPress={()=>{ navigation.navigate('Log In')}}/>
-    </View>
-    <View style={{padding:20}}>
-    <Button  title="Register" onPress={()=>{ navigation.navigate('Register')}}/>
-    </View>
-  </View>
-
-);
-}
+  function LogOrSign({ navigation }) {
+    return (
+      <View>
+        <View style={{ padding: 20 }}>
+          <Button
+            title="Log In "
+            onPress={() => {
+              navigation.navigate("Log In");
+            }}
+          />
+        </View>
+        <View style={{ padding: 20 }}>
+          <Button
+            title="Register"
+            onPress={() => {
+              navigation.navigate("Register");
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
   const [user, setUser] = useState(undefined);
   return (
- 
-<NavigationContainer>
-
-    <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={LogOrSign}           
-          options={{ title: 'Pizza 🍕' }} />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={drinks}
+          options={{ title: "Pizza 🍕" }}
+        />
         <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Log In" component={Login} /> 
-      </Stack.Navigator> 
-  </NavigationContainer>
+        <Stack.Screen name="Log In" component={Login} />
+        <Stack.Screen name="Drinks" component={drinks} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
