dec43d57781f3cf00edce50a7c46b782eddd6d4a
A7med31fimo <ahmedfahim5435644@gmail.com>
2022-05-01 16:58:38 +0200

    maintain errors

--- App.js
+++ App.js
@@ -1,59 +1,26 @@
-import { onAuthStateChanged } from "firebase/auth";
+
 import Login from "./Components/Users/Login";
 import Register from "./Components/Users/Register";
-import { auth } from "./db/Config";
-import { useState, useEffect } from "react";
-
 import Home from "./Components/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Button } from "react-native";
-import Drinks from "./Components/Categories/import_drinks";
+import Drinks from "./Components/Categories/Drinks";
 import FirstPage from "./Components/FirstPage";
 export default function App() {
   const Stack = createNativeStackNavigator();
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => setUser(user));
 
-    return () => {
-      unsub();
-    };
-  }, []);
-  function LogOrSign({ navigation }) {
-    return (
-      <View>
-        <View style={{ padding: 20 }}>
-          <Button
-            title="Log In "
-            onPress={() => {
-              navigation.navigate("Log In");
-            }}
-          />
-        </View>
-        <View style={{ padding: 20 }}>
-          <Button
-            title="Register"
-            onPress={() => {
-              navigation.navigate("Register");
-            }}
-          />
-        </View>
-      </View>
-    );
-  }
-  const [user, setUser] = useState(undefined);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="FirstPage">
         <Stack.Screen
           name="Home"
-          component={FirstPage}
+          component={Home}
           options={{ title: "Pizza 🍕" }}
         />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Log In" component={Login} />
         <Stack.Screen name="Drinks" component={Drinks} />
-        <Stack.Screen name="FirstPage" component={FirstPage} />
+        <Stack.Screen name="FirstPage" component={FirstPage} options={{title:"Getting Start"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
