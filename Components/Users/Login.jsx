1b6edfc1bf72aa1deaca21138dbeb98e3d65ea63
AzizGerges <201927170@std.sci.cu.edu.eg>
2022-05-02 18:03:05 +0200

    edit login and Register design

--- Components/Users/Login.jsx
+++ Components/Users/Login.jsx
@@ -1,65 +1,120 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 import { login } from "../../db/auth/auth";
 // import Register from "./Register";
 // import CitiesList from "../Cities/CitiesList";
 import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../db/Config";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-export default function Login  ({navigation}){
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Background from "../../assets/pizza/logBack.jpg";
+import logo from "../../assets/FirstPage/logo.png";
+export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
   const [error, setError] = useState("");
   return (
-    <View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          padding: 2,
-        }}
+    <View style={styles.container}>
+      <ImageBackground
+        source={Background}
+        resizeMode="cover"
+        style={styles.Background}
       >
-        <Text style={{ flex: 1 }}>Email:</Text>
-        <TextInput
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          style={{ flex: 2, borderColor: "black", borderWidth: 2 }}
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          padding: 2,
-        }}
-      >
-        <Text style={{ flex: 1 }}>Password:</Text>
-        <TextInput
-          onChangeText={setpassword}
-          keyboardType="visible-password"
-          secureTextEntry={true}
-          style={{ flex: 2, borderColor: "black", borderWidth: 2 }}
-        />
-      </View>
-      <View>
-        <Button
-          title="Login"
-          onPress={() => {
-            console.log(email, password);
-            login(email,password)   
-              .then(
-              ()=> { navigation.navigate('Home')} 
-              )
-              .catch((e) => setError(e.message));
-          } 
-        
-        }color="#FB081F"
-        />
-        <Text>{error}</Text>
-      </View>
+        <Image style={styles.logo} source={logo} />
+
+        <View>
+          <TextInput
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            style={styles.inp}
+            placeholder="Email@Example.com"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+          <TextInput
+            onChangeText={setpassword}
+            keyboardType="visible-password"
+            secureTextEntry={true}
+            style={styles.inp}
+            placeholder="Password"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+        </View>
+
+        <View style={styles.foot}>
+          <View style={styles.btn}>
+            <Button
+              title="Log in"
+              onPress={() => {
+                console.log(email, password);
+                login(email, password)
+                  .then(() => {
+                    navigation.navigate("Home");
+                  })
+                  .catch((e) => setError(e.message));
+              }}
+              color="#FB081F"
+            ></Button>
+            <Text>{error}</Text>
+          </View>
+          <Text style={styles.ORtxt}>OR</Text>
+          <View style={styles.btn}>
+            <Button title="Sign up" color="#FB081F"></Button>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  Background: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    marginTop: -60,
+    marginBottom: 20,
+  },
+  inp: {
+    width: 300,
+    height: 40,
+    borderWidth: 2,
+    borderColor: "#FB081F",
+    borderRadius: 10,
+    marginBottom: 30,
+    fontSize: 20,
+    fontStyle: "italic",
+    padding: 6,
+    color: "#FFFFFF",
+  },
+  foot: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  ORtxt: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#FFFFFF",
+    marginVertical: 10,
+  },
+  btn: {
+    width: 250,
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+});
--- Components/Users/Register.jsx
+++ Components/Users/Register.jsx
@@ -1,63 +1,124 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 import { React, useState } from "react";
 import { register } from "../../db/auth/auth";
-
+import Background from "../../assets/pizza/logBack.jpg";
+import logo from "../../assets/FirstPage/logo.png";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
   const [error, setError] = useState("");
 
   return (
-    <View style={{paddingTop:50}}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          padding: 2,
-        }}
+    <View style={styles.container}>
+      <ImageBackground
+        source={Background}
+        resizeMode="cover"
+        style={styles.Background}
       >
-        <Text style={{ flex: 1 }}>Email:</Text>
-        <TextInput
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          style={{ flex: 2, borderColor: "black", borderWidth: 2 }}
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          padding: 2,
-        }}
-      >
-        <Text style={{ flex: 1 }}>Password:</Text>
-        <TextInput
-          onChangeText={setpassword}
-          keyboardType="visible-password"
-          secureTextEntry={true}
-          style={{ flex: 2, borderColor: "black", borderWidth: 2 }}
-        />
-      </View>
-      <View>
-        <Button
-          title="Register"
-          onPress={() => {
-            console.log(email, password);
-            register(email,password)
-            .then(
-              ()=> {
-                navigation.navigate('Home')} 
-              )
-              .catch((e) => setError(e.message));
-          }}
-          color="#FB081F"
-        />
-        <Text>{error}</Text>
-      </View>
+        <Image style={styles.logo} source={logo} />
+
+        <View>
+          <TextInput
+            style={styles.inp}
+            placeholder="Name"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+
+          <TextInput
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            style={styles.inp}
+            placeholder="Email@Example.com"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+
+          <TextInput
+            style={styles.inp}
+            placeholder="Phone"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+
+          <TextInput
+            style={styles.inp}
+            placeholder="Addres"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+
+          <TextInput
+            onChangeText={setpassword}
+            keyboardType="visible-password"
+            secureTextEntry={true}
+            style={styles.inp}
+            placeholder="Password"
+            placeholderTextColor="#FFFFFF"
+          ></TextInput>
+        </View>
+
+        <View style={styles.btn}>
+          <Button
+            title="Register"
+            onPress={() => {
+              console.log(email, password);
+              register(email, password)
+                .then(() => {
+                  navigation.navigate("Home");
+                })
+                .catch((e) => setError(e.message));
+            }}
+            color="#FB081F"
+          ></Button>
+          <Text>{error}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  Background: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    marginTop: -60,
+  },
+  inp: {
+    width: 300,
+    height: 40,
+    borderWidth: 2,
+    borderColor: "#FB081F",
+    borderRadius: 10,
+    marginBottom: 30,
+    fontSize: 20,
+    fontStyle: "italic",
+    padding: 6,
+    color: "#FFFFFF",
+  },
+  foot: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  btn: {
+    marginVertical: 20,
+    width: 250,
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+});
