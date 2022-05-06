2c5ee8f37d8f169f22e76536be1377b7f562cc22
AzizGerges <201927170@std.sci.cu.edu.eg>
2022-05-06 17:54:51 +0200

    edit home page and userinfo

--- Components/Categories/HomePages/Home.jsx
+++ Components/Categories/HomePages/Home.jsx
@@ -6,139 +6,132 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
-
+import { useState } from "react";
 import DealsPage from "../Deals/Deals";
 import PizzaPage from "../pizza/importPizza";
 import CakesPage from "../cake/Cakes";
 import DrinksPage from "../Drinks/Drinks";
 import CartPage from "../CardItems/Card";
+import UserInfo from "../../Users/UserInfo";
 import Icon from "react-native-vector-icons/AntDesign";
-import Icon2 from "react-native-vector-icons/Octicons"
-import { SignOut } from "../../../db/auth/auth";
-
-
 
 export default function Home({ navigation }) {
   const [Page, setPage] = useState(0);
-  const [ColorDeal, setColorDeal] = useState('crimson');
-  const [ColorPizza, setColorPizza] = useState('black');
-  const [ColorCake, setColorCake] = useState('black');
-  const [ColorDrinks, setColorDrinks] = useState('black');
-  const [ColorCard, setColorCard] = useState('black');
+  const [ColorDeal, setColorDeal] = useState("crimson");
+  const [ColorPizza, setColorPizza] = useState("black");
+  const [ColorCake, setColorCake] = useState("black");
+  const [ColorDrinks, setColorDrinks] = useState("black");
+  const [ColorCard, setColorCard] = useState("black");
   const clickDeals = () => {
     setPage(0);
-    setColorDeal('crimson');
-    setColorPizza('black');
-    setColorCake('black');
-    setColorDrinks('black');
-    setColorCard('black');
+    setColorDeal("crimson");
+    setColorPizza("black");
+    setColorCake("black");
+    setColorDrinks("black");
+    setColorCard("black");
   };
   const clickPizza = () => {
     setPage(1);
-    setColorDeal('black');
-    setColorPizza('crimson');
-    setColorCake('black');
-    setColorDrinks('black');
-    setColorCard('black');
+    setColorDeal("black");
+    setColorPizza("crimson");
+    setColorCake("black");
+    setColorDrinks("black");
+    setColorCard("black");
   };
   const clickCakes = () => {
     setPage(2);
-    setColorDeal('black');
-    setColorPizza('black');
-    setColorCake('crimson');
-    setColorDrinks('black');
-    setColorCard('black');
+    setColorDeal("black");
+    setColorPizza("black");
+    setColorCake("crimson");
+    setColorDrinks("black");
+    setColorCard("black");
   };
   const clickDrinks = () => {
     setPage(3);
-    setColorDeal('black');
-    setColorPizza('black');
-    setColorCake('black');
-    setColorDrinks('crimson');
-    setColorCard('black');
+    setColorDeal("black");
+    setColorPizza("black");
+    setColorCake("black");
+    setColorDrinks("crimson");
+    setColorCard("black");
   };
   const clickCart = () => {
     setPage(4);
-    setColorDeal('black');
-    setColorPizza('black');
-    setColorCake('black');
-    setColorDrinks('black');
-    setColorCard('crimson');
+    setColorDeal("black");
+    setColorPizza("black");
+    setColorCake("black");
+    setColorDrinks("black");
+    setColorCard("crimson");
   };
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.categories}>
-        <TouchableOpacity onPress={clickDeals}>
-          <View style={styles.imageview}>
-            <Image
-              style={styles.image}
-              source={{ uri: "https://i.ibb.co/kSf9cpQ/deal4.jpg" }}
-            />
-          </View>
-          <Text style={{ color: ColorDeal, marginLeft: 10 }}>Deals</Text>
-        </TouchableOpacity>
+        <ScrollView horizontal={true}>
+          <TouchableOpacity onPress={clickDeals}>
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/kSf9cpQ/deal4.jpg" }}
+              />
+            </View>
+            <Text style={{ color: ColorDeal, textAlign: "center" }}>Deals</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={clickPizza}>
-          <View style={styles.imageview}>
-            <Image
-              style={styles.image}
-              source={{ uri: "https://i.ibb.co/yWZHMDv/pizza.png" }}
-            />
-          </View>
-          <Text style={{ color: ColorPizza, marginLeft: 10 }}>pizza</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={clickPizza}>
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/yWZHMDv/pizza.png" }}
+              />
+            </View>
+            <Text style={{ color: ColorPizza, textAlign: "center" }}>
+              pizza
+            </Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={clickCakes}>
-          <View style={styles.imageview}>
-            <Image
-              style={styles.image}
-              source={{ uri: "https://i.ibb.co/FVM5YnL/cake.jpg" }}
-            />
-          </View>
-          <Text style={{ color: ColorCake, marginLeft: 10 }}>Cakes</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={clickCakes}>
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/FVM5YnL/cake.jpg" }}
+              />
+            </View>
+            <Text style={{ color: ColorCake, textAlign: "center" }}>Cakes</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={clickDrinks}>
-          <View style={styles.imageview}>
-            <Image
-              style={styles.image}
-              source={{ uri: "https://i.ibb.co/nn7v6MP/images-1.jpg" }}
-            />
-          </View>
-          <Text style={{ color: ColorDrinks, marginLeft: 10 }}>Drinks</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={clickCart}>
-          <View style={styles.imageview}>
-            <Icon name="shoppingcart" size={50} color="crimson" />
-          </View>
-          <Text style={{ color: ColorCard, marginLeft: 10 }}>Cart</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          {
-            SignOut()
-              .then(() => {
-                console.log("sign out")
-                navigation.navigate("FirstPage");
-              }).catch((err) => {
-                setError(err.message)
-              });
-          }
-        }}
-
-
-        >
-          <View style={styles.imageview}>
-            <Icon2 name="sign-out" size={50} color="crimson" />
-          </View>
-          <Text style={{ color: ColorCard, marginLeft: 10 }}>sign-out</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => (navigation.navigate("INFO"))}>
-          <View style={styles.imageview}>
-            <Icon name="user" size={50} color="crimson" />
-          </View>
-          <Text style={{ color: ColorCard, marginLeft: 10 }}>Info</Text>
-        </TouchableOpacity>
+          <TouchableOpacity onPress={clickDrinks}>
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/nn7v6MP/images-1.jpg" }}
+              />
+            </View>
+            <Text style={{ color: ColorDrinks, textAlign: "center" }}>
+              Drinks
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={clickCart}>
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/pKfswCx/cart.png" }}
+              ></Image>
+            </View>
+            <Text style={{ color: ColorCard, textAlign: "center" }}>Cart</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("INFO");
+            }}
+          >
+            <View style={styles.imageview}>
+              <Image
+                style={styles.image}
+                source={{ uri: "https://i.ibb.co/1q25n5P/profile1.png" }}
+              ></Image>
+            </View>
+            <Text style={{ textAlign: "center" }}>Profile</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
       <View style={styles.container}>
         <ScrollView style={{ backgroundColor: "#FFF" }}>
@@ -170,11 +163,9 @@ export default function Home({ navigation }) {
 }
 const styles = StyleSheet.create({
   categories: {
-    flex: 2,
-    alignContent: "center",
-    alignItems: "center",
+    width: "100%",
+    height: 100,
     flexDirection: "row",
-    justifyContent: "space-around",
     backgroundColor: "#FFFFFF",
     borderWidth: 1,
     borderRadius: 10,
@@ -196,18 +187,12 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
     overflow: "hidden",
+    marginHorizontal: 10,
   },
   image: {
     width: 50,
     height: 50,
   },
-  // text: {
-  //   width: "100%",
-  //   fontSize: 16,
-  //   // fontWeight: "bold",
-  //   textAlign: "center",
-
-  // },
   container: {
     flex: 12,
     flexDirection: "column",
--- Components/Users/UserInfo.jsx
+++ Components/Users/UserInfo.jsx
@@ -1,55 +1,187 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    TextInput,
- } from 'react-native'
-import { React, useState ,useEffect} from "react";
-import { auth } from '../../db/Config';
- const UserInfo=()=> {
-    const [email, setEmail] = useState("");
-    const [Name, setName] = useState("");
-function getInfo(){
-    if(auth.currentUser!=null)
-    {setName(auth.currentUser.displayName)
-    setEmail(auth.currentUser.email)
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import { React, useState, useEffect } from "react";
+import { auth } from "../../db/Config";
+import { SignOut } from "../../db/auth/auth";
+const UserInfo = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  function getInfo() {
+    if (auth.currentUser != null) {
+      setName(auth.currentUser.displayName);
+      setEmail(auth.currentUser.email);
     }
-}
-    useEffect(() => {
-        getInfo();
-      }, []);
+  }
+  useEffect(() => {
+    getInfo();
+  }, []);
   return (
-    <View style={styles.body}>
-          <View style={styles.inps}>
-            <Text style={styles.inp}>{Name}</Text>
-            <Text style={styles.inp}>{email}</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.square}>
+          <Text style={styles.texticon}>{Name.charAt(0).toUpperCase()}</Text>
+        </View>
+        <View style={styles.Nameview}>
+          <Text style={styles.Name}>{Name}</Text>
+          <Icon name="edit" size={25} color="grey" />
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.textView}>
+          <Text style={styles.text}>Email:</Text>
+          <Text style={styles.textVal}>{email}</Text>
+          <Icon name="edit" size={20} color="grey" />
+        </View>
+        <View style={styles.textView}>
+          <Text style={styles.text}>Number:</Text>
+          <Text style={styles.textVal}>010000000</Text>
+          <Icon name="edit" size={20} color="grey" />
+        </View>
+        <View style={styles.textView}>
+          <Text style={styles.text}>Address:</Text>
+          <Text style={styles.textVal}>Address Value</Text>
+          <Icon name="edit" size={20} color="grey" />
+        </View>
+        <View style={styles.textView}>
+          <Text style={styles.text}>Password:</Text>
+          <Text style={styles.textVal}>********</Text>
+          <Icon name="edit" size={20} color="grey" />
+        </View>
+        <View style={styles.btns}>
+          <View style={styles.btn}>
+            <Button
+              onPress={() => {
+                navigation.navigate("Home");
+              }}
+              title="Home"
+              color="#FB081F"
+            ></Button>
+          </View>
+          <Text style={styles.ORtxt}>OR</Text>
+          <View style={styles.btn}>
+            <Button
+              onPress={() => {
+                {
+                  SignOut()
+                    .then(() => {
+                      console.log("sign out");
+                      navigation.navigate("FirstPage");
+                    })
+                    .catch((err) => {
+                      setError(err.message);
+                    });
+                }
+              }}
+              title="Log out"
+              color="#FB081F"
+            ></Button>
           </View>
         </View>
-  )
-}
+      </View>
+    </View>
+  );
+};
 export default UserInfo;
 
 const styles = StyleSheet.create({
-    inps: {
-      alignItems: "center",
-      marginTop: 30,
-    },
-    inp: {
-      width: "90%",
-      height: 40,
-      borderWidth: 2,
-      borderColor: "#FB081F",
-      borderRadius: 10,
-      marginBottom: 30,
-      fontSize: 20,
-      fontStyle: "italic",
-      padding: 6,
-      color: "#000000",
-    },
-    btn: {
-      marginVertical: 5,
-      width: "90%",
-      borderRadius: 15,
-      overflow: "hidden",
-    },})
+  container: {
+    flex: 1,
+    backgroundColor: "#F7F7F7",
+    alignItems: "center",
+    marginTop: 30,
+    marginBottom: 40,
+  },
+  header: {
+    flexDirection: "row",
+    margin: 10,
+    backgroundColor: "#FFFFFF",
+    width: "95%",
+    height: 80,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: "#f7eceb",
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+    padding: 7,
+  },
+  square: {
+    width: 60,
+    height: "100%",
+    backgroundColor: "red",
+    borderRadius: 20,
+    justifyContent: "center",
+  },
+  texticon: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  Nameview: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+  Name: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#000000",
+    fontWeight: "bold",
+    marginRight: 20,
+  },
+  body: {
+    paddingTop: 30,
+    margin: 10,
+    backgroundColor: "#FFFFFF",
+    width: "95%",
+    height: 600,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: "#f7eceb",
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  textView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    fontWeight: "bold",
+    alignItems: "center",
+    padding: 10,
+    paddingBottom: 15,
+  },
+  text: {
+    fontSize: 23,
+    textAlign: "center",
+    color: "#000000",
+  },
+  textVal: {
+    fontSize: 15,
+    textAlign: "center",
+    color: "#000000",
+  },
+  ORtxt: {
+    marginVertical: 5,
+    color: "black",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  btns: {
+    paddingTop: 20,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    width: "80%",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+});
