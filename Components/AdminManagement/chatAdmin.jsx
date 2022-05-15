33c2c82e339804af82c2c5f4aee8e1f7cd41a30a
mohamed202122 <moh.essam4100@gmail.com>
2022-05-15 03:07:44 +0200

    some edit 2

--- Components/AdminManagement/chatAdmin.jsx
+++ Components/AdminManagement/chatAdmin.jsx
@@ -1,13 +1,16 @@
-import { View, Text, ScrollView, Button, StyleSheet } from "react-native";
+import { View, Text, ScrollView, Button, StyleSheet , Image } from "react-native";
 import { useState, useEffect } from "react";
 import { getAllChats, subscribe } from "../../db/Edit/chat";
 import Item from "./itemOfCard";
 
 export default function ChatAdmin({ fuc1 }) {
   const [chat, setchats] = useState([]);
+  const [empty, setEmpty] = useState(0);
+
   const getChats = () => {
     getAllChats().then((data) => {
       setchats(data);
+      setEmpty(data.length);
     });
   };
   useEffect(() => {
@@ -23,7 +26,34 @@ export default function ChatAdmin({ fuc1 }) {
     };
   }, []);
   return (
-    <View style={{ flex: 1 }}>
+<View style={{ flex: 1 }}>
+    {
+      empty === 0 ? (
+        <View>
+          <View style={{ alignItems: "center",}}>
+            <Image
+              style={{width: 350, height: 350,}}
+              source={{ uri: "https://i.ibb.co/DKzryP5/logo.png" }}
+            />
+          </View>
+          <Text style={{textAlign: "center", fontSize: 20,fontWeight : "400" ,marginVertical: 10 ,color : "red"}}> 
+            There are no orders now ... 
+            </Text>
+            <Text style ={{textAlign: "center", fontSize: 20, marginVertical: 10}}> have a break </Text>
+          <View style={styles.btnview}>
+            <Button
+              style={styles.btn}
+              title="Explore Menu"
+              color="#FB081F"
+              onPress={() => fuc1()}
+            />
+          </View>
+        </View>
+      )   
+    
+
+      :
+    
       <ScrollView>
         {chat.map((a, index) => (
           <View key={index} style={{ flex: 1, padding: 10 }}>
@@ -49,6 +79,7 @@ export default function ChatAdmin({ fuc1 }) {
           />
         </View>
       </ScrollView>
+      }
     </View>
   );
 }
@@ -65,4 +96,5 @@ const styles = StyleSheet.create({
   btn: {
     width: "50%",
   },
+ 
 });
