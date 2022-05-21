d9cbb9094a3250eb55e123addbafae29d709d27e
mohamed202122 <moh.essam4100@gmail.com>
2022-05-21 22:07:41 +0200

    finish feedBack

--- Components/Users/FeedBack.jsx
+++ Components/Users/FeedBack.jsx
@@ -8,17 +8,141 @@ import {
   ScrollView,
   TextInput,
 } from "react-native";
-import { editFeedBack, AddItemsFeedBack } from "../../db/Edit/FeedBackEdit";
+import { editFeedBack, AddItemsFeedBack , getItemFeedBack} from "../../db/Edit/FeedBackEdit";
 import Icon from "react-native-vector-icons/Entypo";
 import { useState, useEffect } from "react";
 import { auth } from "../../db/Config";
 export default function FeedBack({ navigation }) {
-  const [icon1, setIcon1] = useState("heart-outlined");
-  const [icon2, setIcon2] = useState("heart-outlined");
-  const [icon3, setIcon3] = useState("heart-outlined");
-  const [icon4, setIcon4] = useState("heart-outlined");
-  const [icon5, setIcon5] = useState("heart-outlined");
-  const [number, setNumber] = useState(0);
+
+let y = 0 ; 
+  const getFeedBack = async () => {
+    const c = await getItemFeedBack();
+    
+   
+    function compare(a, b) {
+      if (a.user < b.user ) {
+        return -1;
+      }
+      else if (a.user > b.user ) {
+        return 1;
+      }
+      else { 
+       
+        if (a.date < b.date)
+        return -1 ;
+      
+        else if (a.date > b.date) 
+        return 1  ;
+     
+        else {
+       
+          if (a.hour < b.hour)
+        return -1 ;
+      
+        else if (a.hour > b.hour) 
+        return 1  ;
+      
+        else {
+          if (a.minute < b.minute)
+          return -1 ;
+        
+          else if (a.minute > b.minute) 
+          return 1  ;
+
+          else {
+            if (a.second < b.second)
+          return -1 ;
+        
+          else if (a.second > b.second) 
+          return 1  ;
+
+          else 
+          return 0 ;
+
+          }
+   
+        }
+        
+        }
+      }
+    }
+
+    c.sort(compare);
+
+
+    for (let i = c.length-1 ; i >= 0 ; i--){
+      if (c[i].user === auth.currentUser.displayName){
+          y = c[i].number ;
+          console.log("y : " + y);
+          break ;
+    }
+      }
+      setNumber(y);
+      if (y === 1){
+          setIcon1("heart");
+      }
+      if (y === 2){
+        setIcon1("heart");
+        setIcon2("heart");
+    }
+      if (y === 3){
+        setIcon1("heart");
+        setIcon2("heart");
+        setIcon3("heart");
+    }
+      if (y === 4){
+        setIcon1("heart");
+        setIcon2("heart");
+        setIcon3("heart");
+        setIcon4("heart");
+    }
+      if (y === 5){
+        setIcon1("heart");
+        setIcon2("heart");
+        setIcon3("heart");
+        setIcon4("heart");
+        setIcon5("heart");
+    }
+    setData(c);
+
+  
+    
+  };
+
+  useEffect(() => {
+    getFeedBack();
+  }, []);
+
+  const [Data, setData] = useState("");
+    let x = "" 
+  for (let i = Data.length-1 ; i >= 0 ; i--){
+    console.log("x  : " + x) ;
+    console.log(Data);
+    console.log(auth.currentUser.displayName); 
+    if (Data[i].user === auth.currentUser.displayName){
+
+      x = Data[i].feedBack ; 
+    
+      console.log("x  : " + x) ;
+    
+      break ;
+    }
+  }
+  
+ 
+   
+   
+     const [icon1 , setIcon1] = useState("heart-outlined");
+     const [icon2 , setIcon2] = useState("heart-outlined");
+     const [icon3 , setIcon3] = useState("heart-outlined");
+     const [icon4 , setIcon4] = useState("heart-outlined");
+     const [icon5 , setIcon5] = useState("heart-outlined");
+    
+     const [number, setNumber] = useState(0);
+    const [feedBack, setFeedBack] = useState("");
+  
+  // const [User, setuser] = useState("");
+  
 
   const click1 = () => {
     setNumber(1);
@@ -60,10 +184,10 @@ export default function FeedBack({ navigation }) {
     setIcon4("heart");
     setIcon5("heart");
   };
-  const [FeedBack, setFeedBack] = useState("");
+ 
 
   return (
-    <View>
+    <View style = {{flex : 1}}>
       <View style={styles.logoview}>
         <Image
           style={styles.logo}
@@ -110,12 +234,14 @@ export default function FeedBack({ navigation }) {
         style={{
           height: 100,
           padding: 10,
-          margin: 30,
+          marginHorizontal: 30,
+          marginVertical : 7 ,
           borderWidth: 1,
           borderRadius: 10,
           borderColor: "crimson",
         }}
         placeholder="Leave comment"
+        defaultValue = {x}
         onChangeText={setFeedBack}
       />
 
@@ -126,12 +252,24 @@ export default function FeedBack({ navigation }) {
           color="#FB081F"
           // onPress= {fuc2}
           onPress={() => {
-            AddItemsFeedBack({
-              conatnt: auth.currentUser.displayName + ": " + FeedBack,
-            });
+            let today = new Date();
+            
+            let date = (today.getDate() < 10 ? '0' : '') + today.getDate();
+            let hour = (today.getHours() < 10 ? '0' : '') + today.getHours();
+            let minute = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
+            let second = (today.getSeconds() < 10 ? '0' : '') + today.getSeconds();
+            let month =  today.getMonth();
+            console.log(today + " \n " + date + " \n " + hour + " \n " + minute + " \n " + second + " \n" + month);
+            if (feedBack === "" && x === "")
+                alert("sorry, no feedBack");
+              else if (feedBack === "" && x != "")
+                alert("sorry, add new feedBack");
+              else {
+            AddItemsFeedBack({user: auth.currentUser.displayName ,feedBack , number  , date , hour , minute , second , month});
             navigation.navigate("Home");
             alert("thanks for your FeedBack");
             console.log("feedback");
+              }
           }}
         />
       </View>
@@ -148,7 +286,8 @@ const styles = StyleSheet.create({
     height: 250,
   },
   btnview: {
-    margin: 50,
+    marginHorizontal: 50,
+    marginVertical : 7 ,
     borderRadius: 10,
     overflow: "hidden",
   },
@@ -161,7 +300,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     marginTop: 20,
     flexDirection: "row",
-    // marginHorizontal : 135 ,
     width: "90%",
   },
 });
